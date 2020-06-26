d8ff2f1cbee5cb3cfac44f4c0ebf80b51ab85160
derekserr <57603182+derekserr@users.noreply.github.com>
2020-06-26 16:23:32 -0400

    added a program that uses abstract classes and interfaces to sort nodes into alphabetical order. I have implemented methods that will delete nodes, or add nodes

---
+++ AllClasses/src/abstractChallenge/Main.java
@@ -0,0 +1,17 @@
+package abstractChallenge;
+
+public class Main {
+	public static void main(String[] args) {
+		myLinkedList list=new myLinkedList(null);
+		String stringData="derek kolby kaden sara henry mark john hannah derek";
+		String[]data=stringData.split(" ");
+		for(String s:data) {
+			list.addItem(new Node(s));
+		}
+		list.removeItem(new Node("john"));
+		list.traverse(list.getRoot());
+		System.out.println();
+		System.out.println(list.getRoot().next().next().next().previous().value);
+	}
+
+}
