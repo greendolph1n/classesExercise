089d005ceb8d6d4c152daebfcaac2488965ee9dd
derekserr <57603182+derekserr@users.noreply.github.com>
2020-06-27 13:50:42 -0400

    removed redundant lists

--- AllClasses/src/genericsChallenge/LeagueTable.java
+++ AllClasses/src/genericsChallenge/LeagueTable.java
@@ -1,56 +1,57 @@
 package genericsChallenge;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public class LeagueTable<T extends Team> {
-	private List<Team> mainList = new ArrayList<>();
+	private int numOfTeams=0;
 	private Team root;
 
 	public boolean addTeam(T Team) {
-		if (this.mainList.isEmpty()) {
+		if (this.numOfTeams==0) {
 			this.root = Team;
-			this.mainList.add(this.root);
-			System.out.println(Team.getTeamName() + " has been added.");
+			//System.out.println(Team.getTeamName() + " has been added.");
+			numOfTeams++;
 			return true;
 		}
 		Team current = this.root;
 		while (true) {
-			if (current.getScore() > Team.getScore()) {
+
+			if (current.getTeamName().equals(Team.getTeamName())) {
+				//System.out.println("Team already added");
+				return false;
+			} else if (current.getScore() > Team.getScore()) {
 				if (current.next() == null) {
 					current.setNext(Team).setPrevious(current);
-					mainList.add(Team);
-					System.out.println(Team.getTeamName() + " has been added.");
+				//	System.out.println(Team.getTeamName() + " has been added.");
+					numOfTeams++;
 					return true;
 				}
 				current = current.next();
 			}
 
 			else {
-				if(current==this.root) {
-				
-					if(current.next()==null) {
-					Team.setNext(current).setPrevious(Team);
-					System.out.println(Team.getTeamName() + " has been added.");
-					System.out.println("root is now "+Team.getTeamName());
-					this.root = Team;
-					mainList.add(Team);
-					return true;
+				if (current == this.root) {
+
+					if (current.next() == null) {
+						Team.setNext(current).setPrevious(Team);
+						//System.out.println(Team.getTeamName() + " has been added.");
+						//System.out.println("root is now " + Team.getTeamName());
+						this.root = Team;
+						numOfTeams++;
+						return true;
 					}
 					Team.setNext(current).setPrevious(Team);
-					this.root=Team;
-					mainList.add(Team);
-					System.out.println(Team.getTeamName() + " has been added.");
-					
+					this.root = Team;
+					numOfTeams++;
+					//System.out.println(Team.getTeamName() + " has been added.");
+
 					return true;
 				}
-				System.out.println("replacing "+current.getTeamName()+" with "+Team.getTeamName());
+			//	System.out.println("replacing " + current.getTeamName() + " with " + Team.getTeamName());
 				current.previous().setNext(Team).setNext(current);
 				current.setPrevious(Team);
-				//Team.setPrevious(current.previous());
-				System.out.println(Team.getTeamName() + " has been added.");
-				System.out.println(current.previous().getTeamName());
-				mainList.add(Team);
+				// Team.setPrevious(current.previous());
+			//	System.out.println(Team.getTeamName() + " has been added.");
+				numOfTeams++;
 				return true;
 			}
 
@@ -61,7 +62,7 @@ public class LeagueTable<T extends Team> {
 	public void printTeams() {
 		int count = 0;
 		Team current = this.root;
-		for (int i = 0; i < this.mainList.size(); i++) {
+		for (int i = 0; i < numOfTeams; i++) {
 			count++;
 			System.out.println(count + "." + current.getTeamName());
 			if (current.next() == null) {
@@ -72,4 +73,3 @@ public class LeagueTable<T extends Team> {
 		}
 	}
 }
-
--- AllClasses/src/genericsChallenge/Main.java
+++ AllClasses/src/genericsChallenge/Main.java
@@ -1,12 +1,11 @@
 package genericsChallenge;
 
-import java.util.LinkedList;
-import java.util.List;
 
 public class Main {
 	public static void main(String[] args) {
 		
 		BasketballTeam raptors = new BasketballTeam("Raptors",100);
+		BasketballTeam raptors2 = new BasketballTeam("Raptors2",100);
 		BasketballTeam test2 = new BasketballTeam("test2",12456965);
 		BasketballTeam test3 = new BasketballTeam("test3",3220);
 		BasketballTeam test4 = new BasketballTeam("test4",1002220);
@@ -14,15 +13,13 @@ public class Main {
 		SoccerTeam madrid=new SoccerTeam("Madrid",12);
 		
 		
-		
-		List<BasketballTeam> teamList=new LinkedList<>();
-		
 		LeagueTable <BasketballTeam> table1=new LeagueTable<>();
 		table1.addTeam(raptors);
-		table1.addTeam(test2);
+		//table1.addTeam(test2);
 		table1.addTeam(test4);
 		table1.addTeam(test3);
 		table1.addTeam(test5);
+		table1.addTeam(raptors2);
 		table1.printTeams();
 		
 	}
